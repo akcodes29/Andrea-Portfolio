b4cdf4464ff7a14b3abc2ab2f9376e294fb253cc
Andrea <keriazes.andrea@gmail.com>
2023-11-24 12:22:31 -0500

    added resume link to resume component

--- React-Portfolio/src/Components/Resume.jsx
+++ React-Portfolio/src/Components/Resume.jsx
@@ -4,12 +4,14 @@ import React from 'react'
 const Resume = () => {
     return (
         <div className ='Resume'>
-        <h2>Resume
-        </h2>
+        <h1>Resume
+        </h1>
         <br />
-        <h4>Downloadable resume will go here</h4>
+        <h2> <a href="https://docs.google.com/document/d/1EMFgmtonruV5CbZM3iu266JlQcgv90PEC6dDXlCxQdU/edit?usp=sharing" target="_blank">Click Here To View My Resume</a> </h2>
         <br />
-        {/* <a href={resumeFile} download>Download my resume</a> */}
+        <br />
+        <br />
+
       </div>
     )
 }
